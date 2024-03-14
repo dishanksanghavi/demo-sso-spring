993e73827c3db408d0e6759959ab562a3fa75452
Dishank Sanghavi <dishanksanghavi@Dishanks-MBP.attlocal.net>
2024-03-13 22:54:12 -0400

    Initial commit

---
+++ src/main/java/com/example/demossospring/DemoSsoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demossospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSsoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSsoSpringApplication.class, args);
+	}
+
+}
